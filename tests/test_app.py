889bcda2c2b6d76f4fee2c11495e43eda27bf94a
Nilton Teixeira <9078708+kuresto@users.noreply.github.com>
2021-01-18 22:26:20 -0300

    Created basic testing skeleton for application

---
+++ tests/test_app.py
@@ -0,0 +1,5 @@
+def test_app_init():
+    pass
+
+def test_app_requirements_safety():
+    pass
---
+++ tests/test_queue.py
@@ -0,0 +1,11 @@
+def test_queue_status():
+    pass
+
+def test_basic_queue_entry():
+    pass
+
+def test_queue_retry_backoff():
+    pass
+
+def test_queue_deadletter():
+    pass
