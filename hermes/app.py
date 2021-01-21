aef005a2685e1d42c49fc7aa9d5f1223c85d601b
Nilton Teixeira <9078708+kuresto@users.noreply.github.com>
2021-01-21 13:31:50 -0300

    Moved requirements, added uvicorn server to main for debugging purposes

--- hermes/app.py
+++ hermes/app.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+import uvicorn
 
-app = FastAPI()
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Hermes Messaging API",
+    description="Luiza Labs Code Challenge",
+    docs_url=None,
+    redoc_url="/",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/healthcheck")
 async def healthcheck():
     return {"healthcheck": True}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
