a57e4b7b86cbbddd78604b74b26d096e91bc763b
Nilton Teixeira <9078708+kuresto@users.noreply.github.com>
2021-01-22 02:18:31 -0300

    Added request and responde logging

--- hermes/app.py
+++ hermes/app.py
@@ -1,9 +1,17 @@
+from time import time
+from pprint import pprint
+
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
+from .logs import get_logger
 from .resources import messages_router
+
+
+logger = get_logger(__name__)
+
 
 app = FastAPI(
     title="Hermes Messaging API",
@@ -21,6 +29,22 @@ app.add_middleware(
 )
 
 app.include_router(messages_router)
+
+
+@app.middleware("http")
+async def log_request_and_response_and_timeit(request: Request, call_next):
+    logger.info("Request -> %s", pprint(request.__dict__))
+    start_time = time()
+
+    response = await call_next(request)
+
+    process_time = time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+
+    logger.info("Response -> %s", pprint(response.__dict__))
+    logger.info("Executed in %s", process_time)
+
+    return response
 
 
 @app.get("/healthcheck")
